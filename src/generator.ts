8a1dbd42a2efa6256bbb7d2d87a77c6ac0884d79
Emily Marigold Klassen <forivall@gmail.com>
2020-10-26 20:28:45 -0700

    feat: allow using allOf to create interfaces with extends

--- src/generator.ts
+++ src/generator.ts
@@ -295,6 +295,7 @@ function generateRawType(ast: AST, options: Options): string {
     case 'TYPE_REFERENCE':
       return generateEnumReference(ast)
     default:
+      error('Standalone name ("title") required for item', ast)
       throw unreachableCase(ast)
   }
 }
